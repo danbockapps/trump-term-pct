4f3e8792386a39947c76028b53ca212d397536ca
Dan Bock <danbock@gmail.com>
2017-02-18 09:12:04 -0500

    Add Phirehose and authentication

--- functions.php
+++ functions.php
@@ -1,6 +1,30 @@
 <?php
 date_default_timezone_set('America/New_York');
 
+require_once('libraries/codebird.php');
+$cb = \Codebird\Codebird::getInstance();
+
+foreach(parse_ini_file("auth.ini") as $key => $value) {
+  define($key, $value);
+}
+
+$requiredConstants = array(
+  'TWITTER_CONSUMER_KEY',
+  'TWITTER_CONSUMER_SECRET',
+  'OAUTH_TOKEN',
+  'OAUTH_SECRET',
+);
+
+foreach($requiredConstants as $rc) {
+  if(!defined($rc)) {
+    throw new Exception('Required constant not defined: ' + $rc);
+  }
+}
+
+\Codebird\Codebird::setConsumerKey(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET);
+$cb->setToken(OAUTH_TOKEN, OAUTH_SECRET);
+
+
 function calcPct() {
   $start = strToTime('2017-01-20 12:00:00');
   $end = strToTime('2021-01-20 12:00:00');
